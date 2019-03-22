9698cb446d7cf835c520602d77a5af7d4b8c875f
alumno <alumno@LAB00PC00>
2019-03-21 21:18:09 -0300

    primer commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int pedirNumero (void);
+
+int main()
+{
+    int pedirNumero(void);
+}
+
+int pedirNumero(void)
+{
+    int numero;
+    int maximo;
+    int contador=0;
+    int minimo;
+
+    do
+    {
+        printf("Ingrese numero:");
+        scanf("%d",&numero);
+
+        if(contador ==0)
+        {
+            maximo = numero;
+        }
+        else
+        {
+            if (numero < maximo)
+            {
+                maximo = numero;
+
+            }
+
+            if(numero<maximo)
+            {
+                minimo = numero;
+            }
+        }
+        contador ++;
+    }
+    while(numero!=-1);
+    return 0;
+
+}
